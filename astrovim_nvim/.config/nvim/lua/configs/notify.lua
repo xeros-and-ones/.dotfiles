93c306df1dcef002747d7640eeb12daaccf5299b
Mohamed Tarek <mohamed96tarek@hotmail.com>
2023-10-10 13:09:21 +0300

    changed a lot of stuff in the configs

--- astrovim_nvim/.config/nvim/lua/configs/notify.lua
+++
@@ -1,8 +0,0 @@
-local notify = require "notify"
-notify.setup(
-    astronvim.user_plugin_opts(
-        "plugins.notify",
-        { stages = "fade_in_slide_out", background_colour = "#000000", opacity = 20, timeout = 3000 }
-    )
-)
-vim.notify = notify
