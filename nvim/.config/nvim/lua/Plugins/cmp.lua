53d877b5e55168d57aeb54dec7cf2cae5ace3a6b
Mohamed Tarek <mohamed96tarek@hotmail.com>
2023-10-13 01:52:32 +0300

    asdasdas

--- nvim/.config/nvim/lua/Plugins/cmp.lua
+++ nvim/.config/nvim/lua/Plugins/cmp.lua
@@ -194,6 +194,8 @@ function M.config()
             name = "vim-dadbod-completion",
         } },
     })
+    local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done {})
 end
 
 return M
