51dead95f2ee9249ab1d6de30c39c106c356f7d9
Mohamed Tarek <mohamed96tarek@hotmail.com>
2023-10-26 12:50:40 +0300

    ascasdgjnsfdg

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,11 +1,11 @@
 if vim.g.vscode then
-  require "Core.options"
-  require "Core.keymaps"
+    require "Core.options"
+    require "Core.keymaps"
 else
-  require "Core.options"
-  require "Core.Lazy"
-  require "Core.keymaps"
-  require "Core.autocommands"
+    require "Core.options"
+    require "Core.Lazy"
+    require "Core.keymaps"
+    require "Core.autocommands"
 
-  vim.cmd("colorscheme tokyonight")
+    vim.cmd("colorscheme gruvbox")
 end
