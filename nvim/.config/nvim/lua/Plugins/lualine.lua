7b4b236f10eb201c0925bdf0500fb288343ffa41
Mohamed Tarek <mohamed96tarek@hotmail.com>
2023-10-26 12:11:09 +0300

    asdasdasd

--- nvim/.config/nvim/lua/Plugins/lualine.lua
+++ nvim/.config/nvim/lua/Plugins/lualine.lua
@@ -83,7 +83,7 @@ function M.config()
     lualine.setup {
         options = {
             icons_enabled = true,
-            theme = "powerline_custom",
+            theme = "powerline_gruvbox",
             component_separators = { left = "┃", right = "┃" },
             section_separators = { left = "", right = "" },
             disabled_filetypes = {
