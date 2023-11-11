f7e210c02c468af6f6a831f19ae3ed98ec10c317
Mohamed Tarek <mohamed96tarek@hotmail.com>
2023-11-11 02:24:12 +0200

    ajslkdjasldk

--- XeroChad/.config/nvim/lua/custom/init.lua
+++ XeroChad/.config/nvim/lua/custom/init.lua
@@ -6,9 +6,10 @@ require("custom.core.utilities")
 if vim.g.neovide then
 	vim.opt.guifont = "FiraCode Nerd Font:h11"
 	vim.g.neovide_transparency = 0.8
-	vim.g.neovide_floating_blur_amount_x = 2.0
-	vim.g.neovide_floating_blur_amount_y = 2.0
-
+	-- vim.g.neovide_floating_blur_amount_x = 2.0
+	-- vim.g.neovide_floating_blur_amount_y = 2.0
+	-- vim.g.neovide_refresh_rate = 100
+	-- vim.g.neovide_refresh_rate_idle = 5
 	local function toggle_transparency()
 		if vim.g.neovide_transparency == 1.0 then
 			vim.cmd("let g:neovide_transparency=0.8")
