a7b3f2131bec1ead2b90992a83d5dfd6c2490541
Mohamed Tarek <mohamed96tarek@hotmail.com>
2023-11-15 13:19:45 +0200

    just added the doom private config

---
+++ doom-config/.config/doom/config.el
@@ -0,0 +1,32 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq user-full-name "Mohamed Tarek"
+      user-mail-address "mohamed96tarek@hotmail.com")
+;;------------------------ fonts -----------------------------
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 14 :weight 'semibold)
+      doom-variable-pitch-font (font-spec :family "Rubik" :size 15 :weight 'medium))
+
+;; --------------------- Theme -----------------------------
+(setq doom-theme 'doom-gruvbox)
+
+
+;; --------------------- Options --------------------------
+(setq display-line-numbers-type 'relative)
+(setq org-directory "~/org/")
+
+(setq native-comp-jit-compilation t)
+(add-to-list 'default-frame-alist '(alpha-background . 80)) ; For all new frames henceforth
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+(setq confirm-kill-emacs nil) ; Disable exit confirmation.
+
+;; ------------------ Mappings --------------------------
+(map! :leader
+      :desc "Open Treemacs" "e" #'treemacs)
---
+++ doom-config/.config/doom/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" "3de5c795291a145452aeb961b1151e63ef1cb9565e3cdbd10521582b5fd02e9a" "dc8285f7f4d86c0aebf1ea4b448842a6868553eded6f71d1de52f3dcbc960039" default))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
