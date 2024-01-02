700fd482c7ea86cfb9684793f8d579ac015948ba
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-01-02 19:25:56 +0200

    emacs-config testing some stuff

--- doom-config/.config/doom/+git.el
+++ doom-config/.config/doom/+git.el
@@ -4,43 +4,6 @@
 ;; GIT
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(after! git-link
-  (setq git-link-open-in-browser nil
-        git-link-use-commit t)
-
-  (add-to-list 'git-link-remote-alist
-               '("git\\.amazon\\.com" git-link-amazon-code))
-  (add-to-list 'git-link-commit-remote-alist
-               '("git\\.amazon\\.com" git-link-commit-amazon-code))
-  (add-to-list 'git-link-remote-alist
-               '("amazonaws\\.com" git-link-aws-codecommit))
-  (add-to-list 'git-link-commit-remote-alist
-               '("amazonaws\\.com" git-link-commit-aws-codecommit))
-
-  ;; For some company still uses http git server
-  ;; (add-to-list 'git-link-remote-alist
-  ;;              '("git\\.dummy\\.com" git-link-github-http))
-  ;; (add-to-list 'git-link-commit-remote-alist
-  ;;              '("git\\.dummy\\.com" git-link-commit-github-http))
-
-  ;; OVERRIDE
-  (advice-add #'git-link--select-remote :override #'git-link--read-remote))
-
-
-(after! magit
-  (setq magit-save-repository-buffers nil
-        git-commit-style-convention-checks nil
-        magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
-
-  ;; Add git-credential-manager-core support
-  (add-hook 'magit-process-prompt-functions
-            'magit-process-git-credential-manager-core)
-
-  ;; fix magit prompt for midway auth
-  (appendq! magit-process-password-prompt-regexps '("Kerberos authentication failed.  Password:"))
-
-  (magit-wip-after-apply-mode t)
-  (magit-wip-before-change-mode t))
 
 
 (after! forge
