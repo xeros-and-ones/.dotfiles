db411c1001112cc1dc5cec74df6ca87b11a87dfe
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-01-04 02:29:20 +0200

    rewritten doom config

--- doom-config/.config/doom/modules/private/my-checker/packages.el
+++
@@ -1,6 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; private/my-checker/packages.el
-
-;; (package! wucuo)
-(when (modulep! :checkers syntax)
-  (package! flycheck-google-cpplint :recipe (:host github :repo "flycheck/flycheck-google-cpplint")))
--- doom-config/.config/doom/modules/private/nano-theme/packages.el
+++
@@ -1,4 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; private/nano-theme/packages.el
-
-(package! nano-emacs :recipe (:host github :repo "rougier/nano-emacs"))
