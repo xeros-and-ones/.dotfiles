4eeb2e9b53749240b198fc3af2c608cfd90ad257
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-02-19 09:52:29 +0200

    try some stuff in emacs-from-scratch

--- emacs-from-scratch/.config/emacs/init.el
+++ emacs-from-scratch/.config/emacs/init.el
@@ -1,12 +1,12 @@
 ;;; init.el --- Personal configuration file -*- lexical-binding: t; no-byte-compile: t; -*-
 ;; NOTE: init.el is now generated from readme.org.  Please edit that file instead
 
-  (defconst config-org (locate-user-emacs-file "README.org"))
-  (defconst config-el (locate-user-emacs-file "config.el"))
+(defconst config-org (locate-user-emacs-file "README.org"))
+(defconst config-el (locate-user-emacs-file "config.el"))
 
-  (unless (file-exists-p config-el)
-    (require 'org)
-    (org-babel-tangle-file config-org config-el))
+(unless (file-exists-p config-el)
+(require 'org)
+(org-babel-tangle-file config-org config-el))
 
-  (load-file config-el)
+(load-file config-el)
 ;;; init.el ends here
